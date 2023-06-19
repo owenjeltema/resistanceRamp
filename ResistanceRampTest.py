32841682caf428dac3433c51a31b1e594a8e0888
dylanmount <dcm22@calvin.edu>
2023-06-19 16:56:49 -0400

    new changes

--- ResistanceRampTest.py
+++ ResistanceRampTest.py
@@ -4,17 +4,31 @@ import matplotlib.pyplot as plt
 import pyabf.filter
 import numpy as np
 
+
 #Filters the data and returns and list with the nan removed 
 # a is the array that will be filtered 
-def filter(a):
+def low_pass(a):
+    #aplies the low pass
     sigma=10 # change this value to increase or decrease the level of filtering 
     pyabf.filter.gaussian(abf, 0)  # remove old filter
     pyabf.filter.gaussian(abf, sigma)  # apply custom sigma
     abf.setSweep(0)  # reload sweep with new filter
    
+   #removes the values that are associted with NaN
     indices = np.logical_not(np.isnan(abf.sweepY))
     a = a[indices]
     return a
+
+#Removes the data that is outside the lower and upper bound
+# list is the list that needs to be filtered
+def filter(lower, upper,list):
+   #removes the values that are associted with NaN
+   low = np.where(abf.sweepX == lower)[0][0]
+   up = np.where(abf.sweepX == upper)[0][0]
+   print(low,up)
+   #list=list[low:up]
+   #return list
+
 # zoom in on an interesting region and decorate the plot
 def graph():
     plt.title("Gaussian Filtering of ABF Data")
@@ -24,18 +38,48 @@ def graph():
     plt.legend()
     plt.show()
 
+#quadric
+# TO-Do select only the resistnace part of the graph
+# create somethig to analysis model 
+def n_reg(time,current,n):
+    #polynomial fit with degree = 2
+    #x,y, n
+    model = np.poly1d(np.polyfit(time, current, n))
+
+    #add fitted polynomial line to scatterplot
+    polyline = np.linspace(1, 60, 50)
+    plt.scatter(time, current)
+    plt.plot(polyline, model(polyline))
+    plt.show()
+    print(model)
+
 
 # Brings in the file
 abf = pyabf.ABF("23616002.abf")
-plt.figure(figsize=(8, 5))
+plt.figure(figsize=(8, 5))    #this might not need to be here 
+
+
+
+#testing filter
+current_time = filter(20, 50, abf.sweepY)
+print("len", len(current_time))
+print(current_time)
+print(abf.sweepX)
+
+
+
+
+#quad_reg
+#n_reg(abf.sweepX,abf.sweepY,2)
 
 # plot the original data
 abf.setSweep(0)
 plt.plot(abf.sweepX, abf.sweepY, alpha=0.3, label="original")
 
+
 #Filter the data 
-currentFiltered = filter(abf.sweepY)
-timeFiltered =filter(abf.sweepX)
+currentFiltered = low_pass(abf.sweepY)
+timeFiltered =low_pass(abf.sweepX)
 
 #plot the filtered data 
 abf.setSweep(0)  # reload sweep with new filter
