811facbf8c0a581f7bf5827589030b71fc61304f
dylanmount <dcm22@calvin.edu>
2023-06-19 15:32:47 -0400

    switched order

--- ResistanceRampTest.py
+++ ResistanceRampTest.py
@@ -4,17 +4,6 @@ import matplotlib.pyplot as plt
 import pyabf.filter
 import numpy as np
 
-
-
-
-# Brings in the file
-abf = pyabf.ABF("23616002.abf")
-plt.figure(figsize=(8, 5))
-
-# plot the original data
-abf.setSweep(0)
-plt.plot(abf.sweepX, abf.sweepY, alpha=0.3, label="original")
-
 #Filters the data and returns and list with the nan removed 
 # a is the array that will be filtered 
 def filter(a):
@@ -26,22 +15,8 @@ def filter(a):
     indices = np.logical_not(np.isnan(abf.sweepY))
     a = a[indices]
     return a
-    
-
-currentFiltered = filter(abf.sweepY)
-timeFiltered =filter(abf.sweepX)
-#need to place somewhere else possibily 
-abf.setSweep(0)  # reload sweep with new filter
-label = "sigma: %.02f" % (10)
-plt.plot(abf.sweepX, abf.sweepY, alpha=.8, label=label) #this will need to change later 
-
-print(currentFiltered)
-print(timeFiltered)
-print("len Y:", len(currentFiltered)) # prints all of the current data 
-print("len X ", len(timeFiltered)) #prints all of the time
-
+# zoom in on an interesting region and decorate the plot
 def graph():
-    # zoom in on an interesting region and decorate the plot
     plt.title("Gaussian Filtering of ABF Data")
     plt.ylabel(abf.sweepLabelY)
     plt.xlabel(abf.sweepLabelX)
@@ -49,4 +24,33 @@ def graph():
     plt.legend()
     plt.show()
 
+
+# Brings in the file
+abf = pyabf.ABF("23616002.abf")
+plt.figure(figsize=(8, 5))
+
+# plot the original data
+abf.setSweep(0)
+plt.plot(abf.sweepX, abf.sweepY, alpha=0.3, label="original")
+
+#Filter the data 
+currentFiltered = filter(abf.sweepY)
+timeFiltered =filter(abf.sweepX)
+
+#plot the filtered data 
+abf.setSweep(0)  # reload sweep with new filter
+label = "sigma: %.02f" % (10)
+plt.plot(abf.sweepX, abf.sweepY, alpha=.8, label=label) 
+
+#Create a graph of the pre and post filter data 
 graph()
+
+'''
+print(currentFiltered)
+print(timeFiltered)
+print("len Y:", len(currentFiltered)) # prints all of the current data 
+print("len X ", len(timeFiltered)) #prints all of the time
+'''
+
+
+
