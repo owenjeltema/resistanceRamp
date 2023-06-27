2f9d145b3503b0e7edef42a07de469d70c612c37
dylanmount <dcm22@calvin.edu>
2023-06-27 16:01:04 -0400

    fixed graph

--- ResistanceRampTest.py
+++ ResistanceRampTest.py
@@ -7,8 +7,6 @@ from statistics import mean
 
 
 #TO-Do 
-#split lowapss into 2 functions??
-#fix n_reg
 #fix y axis to fit the graph 
 #figure out time better
 
@@ -37,28 +35,42 @@ def filter(lower, upper,list):
    return list
 
 # zoom in on an interesting region and decorate the plot
-def graph():
+def graph(y1,y2):
     plt.title("Gaussian Filtering of ABF Data")
     plt.ylabel(abf.sweepLabelY)
-    plt.xlabel(abf.sweepLabelX)
-    plt.axis([0, 150, -5, 10])
+    plt.xlabel(abf.sweepLabelX)                                         
+    plt.axis([0, 160, y1, y2])
     plt.legend()
     plt.show()
 
 #quadric
 # TO-Do select only the resistnace part of the graph
 # create somethig to analysis model 
-def n_reg(time,current,n):
+def n_reg(voltage,current,n):
     #polynomial fit with degree = 2
     #x,y, n
-    model = np.poly1d(np.polyfit(time, current, n))
+    model = np.poly1d(np.polyfit(voltage, current, n))
 
     #add fitted polynomial line to scatterplot
-    polyline = np.linspace(1, 15000, 50)
-    plt.scatter(time, current)
+    polyline = np.linspace(1, 160, 50)
+    plt.scatter(voltage, current)
     plt.plot(polyline, model(polyline))
     plt.show()
     print(model)
+
+    #find the r-squared 
+    results={}
+    yhat = model(voltage)
+    ybar = np.sum(current)/len(current)
+    ssreg = np.sum((yhat-ybar)**2)
+    sstot = np.sum((current - ybar)**2)
+    results['r_squared'] = ssreg / sstot
+    print(results)
+
+    #find the slope at zero 
+    print("condutance nS",model[1],"resistacne",pow(model[1],-1))
+
+
 
 
 #takes the last n seconds of the graph and takes the average of the current 
@@ -66,10 +78,10 @@ def n_reg(time,current,n):
 #to-dp
 def averageI():
     a =[]
-    indexList=[990,1990,2990,3990,4990,5990,6990,7990,8990,9990,10990,11990,12990,13990,14990]
+    indexList=[990,1990,2990,3990,4990,5990,6990,7990,8990,9990,10990,11990,12990,13990,14990,15990]
     for x in indexList:
         print(x)
-        holdlist=abf.sweepY[x-50:x]
+        holdlist=abf.sweepY[x-200:x]
         print(mean(holdlist))
         a.append(mean(holdlist))
     return a 
@@ -86,28 +98,25 @@ plt.figure(figsize=(8, 5))    #this might not need to be here
 test =[]
 test= averageI()
 print(test)
-indexList=[990,1990,2990,3990,4990,5990,6990,7990,8990,9990,10990,11990,12990,13990,14990]
+indexList=[0,10,15,20,30,40,50,60,70,80,90,100,120,130,140,150]
 n_reg(indexList,test,2)
 
-#quad_reg
-#n_reg(abf.sweepX,abf.sweepY,2)
 
 # plot the original data
 abf.setSweep(0)
-plt.plot(abf.sweepX, abf.sweepY, alpha=0.3, label="original")
+plt.plot(abf.sweepX, abf.sweepY, alpha=0.3,label='original')
 
-
-#Filter the data 
-current_lowpass = low_pass(abf.sweepY)
-time_lowpass =low_pass(abf.sweepX)
-
+'''
 #plot the filtered data 
 abf.setSweep(0)  # reload sweep with new filter
 label = "sigma: %.02f" % (10)
 plt.plot(abf.sweepX, abf.sweepY, alpha=.8, label=label) 
+'''
+
+
 
 #Create a graph of the pre and post filter data 
-graph()
+graph(abf.sweepY[0]-2,abf.sweepY[15990]+2)
 
 
 
