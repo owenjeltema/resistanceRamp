2534c463e251a7b0f8d4539ac3826f009037ca71
dylanmount <dcm22@calvin.edu>
2023-06-27 09:56:56 -0400

    added averageI

--- ResistanceRampTest.py
+++ ResistanceRampTest.py
@@ -29,9 +29,10 @@ def low_pass(a):
 #Removes the data that is outside the lower and upper bound
 # list is the list that needs to be filtered
 def filter(lower, upper,list):
-   #removesthe values that are associted with NaN
+   #removes the values that are associted with NaN
    low = np.where(abf.sweepX == lower)[0][0]
    up = np.where(abf.sweepX == upper)[0][0]
+   print("low",low,"high",up)
    list=list[low:up]
    return list
 
@@ -40,7 +41,7 @@ def graph():
     plt.title("Gaussian Filtering of ABF Data")
     plt.ylabel(abf.sweepLabelY)
     plt.xlabel(abf.sweepLabelX)
-    plt.axis([9.99, 20, -5, 10])
+    plt.axis([0, 150, -5, 10])
     plt.legend()
     plt.show()
 
@@ -53,7 +54,7 @@ def n_reg(time,current,n):
     model = np.poly1d(np.polyfit(time, current, n))
 
     #add fitted polynomial line to scatterplot
-    polyline = np.linspace(1, 60, 50)
+    polyline = np.linspace(1, 15000, 50)
     plt.scatter(time, current)
     plt.plot(polyline, model(polyline))
     plt.show()
@@ -64,12 +65,24 @@ def n_reg(time,current,n):
 #returns a list of the averages at each step
 #to-dp
 def averageI(n):
-    timeList= [9.5, 19.5] #all 15 steps
-    a = []
+    a =[]
+    indexList=[990,1990,2990,3990,4990,5990,6990,7990,8990,9990,10990,11990,12990,13990,14990]
+    '''
+    timeList= [7.5,9.5] #all 15 steps
+    #holdList = filter(8,9,abf.sweepY)
     for x in timeList:
+        print(x)
         holdList = filter(x-n,x,abf.sweepY)
-        a.append(mean(holdList))
+        print(mean(holdList))
+        #a.append(mean(holdList))
     return a
+'''
+    for x in indexList:
+        print(x)
+        holdlist=abf.sweepY[x-200:x]
+        print(mean(holdlist))
+        a.append(mean(holdlist))
+    return a 
 
 
 #returns the index of a specific time
@@ -80,8 +93,11 @@ abf = pyabf.ABF("23626001.abf")
 plt.figure(figsize=(8, 5))    #this might not need to be here 
 
 #testing average 
-test=averageI(1.6)
+test =[]
+test= averageI(1.6)
 print(test)
+indexList=[990,1990,2990,3990,4990,5990,6990,7990,8990,9990,10990,11990,12990,13990,14990]
+n_reg(indexList,test,2)
 
 #quad_reg
 #n_reg(abf.sweepX,abf.sweepY,2)
