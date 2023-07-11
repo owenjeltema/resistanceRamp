dab416f9f6afb120ca8ff6d4751aa294705e437f
L0wk <sebastiangrabill03@gmail.com>
2023-07-11 10:03:00 -0400

    "update"

--- ResistanceRampTest.py
+++ ResistanceRampTest.py
@@ -1,13 +1,9 @@
-# add significance testing that Haarsma wanted
-# read in files and itterate, and putting results into results folder. (good quality of life improvements.)
-
 import pyabf
 import math
 import matplotlib.pyplot as plt
 import pyabf.filter
 import numpy as np
 from statistics import mean
-import pandas as pd
 import os
 import math
 
@@ -21,6 +17,25 @@ import math
 # fix y axis to fit the graph
 # figure out time better
 
+voltageList = [
+            0,
+            10,
+            20,
+            30,
+            40,
+            50,
+            60,
+            70,
+            80,
+            90,
+            100,
+            110,
+            120,
+            130,
+            140,
+            150,
+        ]
+
 
 def calculate_standard_error(data):
     n = len(data)
@@ -33,7 +48,7 @@ def calculate_standard_error(data):
 
 # Filters the data and returns and list with the nan removed
 # a is the array that will be filtered
-def low_pass(a):
+def low_pass(listToFilter):
     # aplies the low pass
     sigma = 10  # change this value to increase or decrease the level of filtering
     pyabf.filter.gaussian(abf, 0)  # remove old filter
@@ -41,9 +56,9 @@ def low_pass(a):
     abf.setSweep(0)  # reload sweep with new filter
 
     # removes the values that are associted with NaN
-    indices = np.logical_not(np.isnan(abf.sweepY))
-    a = a[indices]
-    return a
+    listWithoutNan = np.logical_not(np.isnan(abf.sweepY))
+    listToFilter = listToFilter[listWithoutNan]
+    return listToFilter
 
 
 # Removes the data that is outside the lower and upper bound
@@ -52,7 +67,6 @@ def filter(lower, upper, list):
     # removes the values that are associted with NaN
     low = np.where(abf.sweepX == lower)[0][0]
     up = np.where(abf.sweepX == upper)[0][0]
-    # print("low", low, "high", up)
     list = list[low:up]
     return list
 
@@ -70,9 +84,6 @@ def n_reg(voltage, current, n, range):
     # add fitted polynomial line to orignal data
     fig = plt.figure()
     ax = fig.add_subplot(111, label="1")
-    # ax2=fig.add_subplot(111,label="2", frame_on=False)
-
-    # fig, (ax, ax2) = plt.subplots(2, sharey=True)
 
     # original data
     ax.plot(abf.sweepX, abf.sweepY, alpha=0.3, label="original")
@@ -94,7 +105,6 @@ def n_reg(voltage, current, n, range):
     plt.plot(polyline, model(polyline))
 
     plt.show()
-    # print(model)
 
     # find the r-squared
     results = {}
@@ -113,9 +123,9 @@ def n_reg(voltage, current, n, range):
 # returns a list of the averages at each step
 # amount is the amount of time that is used to get data from
 # to-dp
-def averageI(list, amount):
-    a = []
-    indexList = [
+def averageI(list, timeshift):
+    AverageTimeShiftDataAllSteps = []
+    timeArray = [
         990,
         1990,
         2990,
@@ -133,55 +143,19 @@ def averageI(list, amount):
         14990,
         15990,
     ]
-    for x in indexList:
-        # print(x)
-        holdlist = list[x - amount : x]
-        # print("step current",mean(holdlist))
-        a.append(mean(holdlist))
-    return a
+    for x in timeArray:
+        TimeShiftData = list[x - timeshift : x]
+        AverageTimeShiftDataAllSteps.append(mean(TimeShiftData))
+    return AverageTimeShiftDataAllSteps
 
 
-def filtered_average(list, range):
-    time = [9.9, 19.9]
-    # ,29.9,39.9,49.9,59.9,69.9,79.9,89.9,99.9,109.9,119.9,129.9,139.9,149.9]
-    stepCurrent = []
-    for x in time:
-        hold = filter(x - range, x, list)
-        stepCurrent.append(mean(hold))
-        print(mean(hold))
-    return stepCurrent
-
-
-# Brings in the file
-# abf = pyabf.ABF("23623002.abf")
-# plt.figure(figsize=(8, 5))  # this might not need to be here
-
-# iterate through all files within directory that IDE is currently within. Set path to be wherever dataset is.
+# iterate through all files within directory that IDE is currently within. Set path to be wherever dataset is for easy itteration and saving.
 for file in os.listdir():
     # Check whether file is in abf format or not
     if file.endswith(".abf"):
         # call read abf file
         abf = pyabf.ABF(file)
         stepCurrent = averageI(abf.sweepY, 100)
-        # print(stepCurrent)
-        voltageList = [
-            0,
-            10,
-            20,
-            30,
-            40,
-            50,
-            60,
-            70,
-            80,
-            90,
-            100,
-            110,
-            120,
-            130,
-            140,
-            150,
-        ]
         # larger scale (better for higher temps)
         print("enter voltages that should be removed, n = no data")
         print("Standard Error: ", calculate_standard_error(stepCurrent), file)
@@ -197,34 +171,6 @@ for file in os.listdir():
         print("Standard Error: ", calculate_standard_error(stepCurrent), file)
         n_reg(voltageList, stepCurrent, 1, 1)
 
-
-# testing average
-
-# stepCurrent = averageI(abf.sweepY, 100)
-# print(stepCurrent)
-# voltageList = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-# # bigger scale (better for higher temps)
-# n_reg(voltageList, stepCurrent, 1, 2)
-
-# code to fix scale                                                              to-do
-
-# print("enter voltages that should be removed, n = no data")
-# remove = input("Enter values:")
-# if remove != "n":
-#     delete = remove.split()
-#     for x in delete:
-#         a = int(x)
-#         index = voltageList.index(a)
-#         voltageList.pop(index)
-#         stepCurrent.pop(index)
-# # print(stepCurrent)
-# n_reg(voltageList, stepCurrent, 1, 1)
-
-# plot filtered data
-# filteredCurrent=low_pass(abf.sweepY)
-# filteredTime=low_pass(abf.sweepX)
-# filteredStepCurrent=filtered_average(filteredCurrent,2)
-# n_reg(voltageList,filteredStepCurrent,2,2)
 
 
 # plot the original data
