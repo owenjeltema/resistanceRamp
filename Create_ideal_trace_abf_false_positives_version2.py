1ddb75a43ee09c3c6c333c0183d79f87d8a280d4
L0wk <sebastiangrabill03@gmail.com>
2024-05-29 16:08:54 -0400

    Set Level Revamp

--- Create_ideal_trace_abf_false_positives_version2.py
+++ Create_ideal_trace_abf_false_positives_version2.py
@@ -59,173 +59,68 @@ pA_levels = [
 #######################################################################################
 
 
+import numpy as np
+
+
 def set_level(abf_file):
-    # function to determine if two values are in the same bin
-    # window_width= 59.999
-    global window_width
+    global window_width, abf, duration, time_step, filtered_sweepY, net_mean
+    global level, bin_index, bin_mean, bin_length
+
     window_width = 10
 
-    # the abf file
-    global abf
+    # Load the ABF file and process it
     abf = pyabf.ABF(abf_file)
-    global duration
     duration = abf.sweepLengthSec
-
-    # do you want to just display the graph with just the filtered data and horizontal lines at bin juctions? (to check if the bin locations are appropriate) (yes/no)
-    global time_step
     time_step = duration / len(abf.sweepY)
     abf.sweepY = -1.0 * abf.sweepY
 
-    # low pass filter
-
-    global filtered_sweepY
+    # Apply a low-pass filter
     filtered_sweepY = low_pass(abf.sweepY, abf.sweepX, cutoff, time_step)
 
-    # defining lists
-    global level
-    level = []
-    global bin_index
-    bin_index = []
-    global bin_mean
-    bin_mean = []
-    global bin_length
-    bin_length = []
+    # Initialize result lists
+    level, bin_index, bin_mean, bin_length = [], [], [], []
+
+    # Determine bin indices using vectorized operations
+    bins_list_np = np.array(bins_list)
+    bin_indices = np.digitize(filtered_sweepY, bins_list_np)
+
+    # Track the current bin
+    bin_start = 0
     bin_current = []
-    # if it isn't checking the bin locations, then it does the rest of the program
-    # v is the index location in the larger list, 0 through 600,000
-    v = 0
-    while v < len(filtered_sweepY):
-        # print(v)
-        # w is the bin number
-        w = 1
-        while w <= len(bins_list) and v < len(filtered_sweepY):
-            # print(w)
-            # runs loop if v less than the first bin junction (closed current), or if it is in bin w. If it is not, then w is increased by one and it tries again until the correct bin has been found
-            while v < len(filtered_sweepY) and (
-                (filtered_sweepY[v] <= bins_list[0])
-                or (w == len(bins_list) and filtered_sweepY[v] >= bins_list[w - 1])
-                or (
-                    filtered_sweepY[v] > bins_list[w - 1]
-                    and filtered_sweepY[v] <= bins_list[w]
-                )
-            ):
-                # when t equals 0 it indicates that the list for a certain level is not yet complete
-                t = 0
-                # creates a list (bin_current) of all the values in a row in the same bin
-                bin_current.append(filtered_sweepY[v])
-                v = v + 1
-                # if the value at location v is not in the same bin as the one at location v-1, then that is either the start of a new event or noise
-                # if the length of bin current is greater than (1/ cutoff* 1/time_step), then it follows this piece of code
-                if (
-                    v == len(filtered_sweepY)
-                    or len(bin_mean) == 0
-                    or (
-                        same_bin(filtered_sweepY[v], filtered_sweepY[v - 1], bins_list)
-                        != True
-                        and (
-                            (
-                                same_bin(filtered_sweepY[v], bin_mean[-1], bins_list)
-                                == True
-                                and len(bin_current) >= (1 / cutoff) * (1 / time_step)
-                            )
-                            or same_bin(filtered_sweepY[v], bin_mean[-1], bins_list)
-                            != True
-                            and len(bin_current) >= 2 * (1 / cutoff) * (1 / time_step)
-                        )
-                    )
-                    and t == 0
-                ):
-                    # adds the length, mean, index, and level number of that section of code onto a list
-                    bin_length.append(len(bin_current))
-                    bin_mean.append(mean(bin_current))
-                    bin_index.append(v - len(bin_current))
-                    if filtered_sweepY[v - 1] <= bins_list[0]:
-                        # print("appended 0 to level")
-                        level.append(0)
-                    if w < len(bins_list) and (
-                        filtered_sweepY[v - 1] > bins_list[w - 1]
-                        and filtered_sweepY[v - 1] <= bins_list[w]
-                    ):
-                        # print("appended w to level, version 1")
-                        level.append(w)
-                    if (
-                        w == len(bins_list)
-                        and filtered_sweepY[v - 1] >= bins_list[w - 1]
-                    ):
-                        # print("appended w to level, version 2")
-                        level.append(w)
-                    # t=1 indicates that the list for a level has been completed
-                    t = 1
 
-                # runs this bit of code if there has been a transition that is less than ____ and after the event it returns to the same level (a simple noise spike)
-                if (
-                    v < len(filtered_sweepY)
-                    and (
-                        (len(bin_current) < ((1 / cutoff) * (1 / time_step)))
-                        and (
-                            same_bin(
-                                filtered_sweepY[v], filtered_sweepY[v - 1], bins_list
-                            )
-                            != True
-                        )
-                        and (
-                            same_bin(filtered_sweepY[v], bin_mean[-1], bins_list)
-                            == True
-                        )
-                    )
-                    and (t == 0)
-                ):
-                    # adds the length and means from the last section to the length and mean from the most recent section (with weighting for the mean)
-                    # then removes the previous values from the lists and adds the new ones instead
-                    new_length = bin_length[-1] + len(bin_current)
-                    bin_length.pop()
-                    bin_length.append(new_length)
-                    if len(bin_current) > 1:
-                        mean_bin = mean(bin_current)
-                    if len(bin_current) == 1:
-                        mean_bin = bin_current[0]
-                    new_mean = (
-                        bin_mean[-1] * bin_length[-1] + mean_bin * len(bin_current)
-                    ) / (bin_length[-1] + len(bin_current))
-                    bin_mean.pop()
-                    bin_mean.append(new_mean)
-                    t = 1
-                # if the potential event is less than the decided length and the level before it is not the same as the level after it, then it follows this code
-                if (
-                    v < len(filtered_sweepY)
-                    and (
-                        (len(bin_current) < (2 * (1 / cutoff) * (1 / time_step)))
-                        and (
-                            same_bin(
-                                filtered_sweepY[v], filtered_sweepY[v - 1], bins_list
-                            )
-                            != True
-                        )
-                    )
-                    and (same_bin(filtered_sweepY[v], bin_mean[-1], bins_list) != True)
-                    and (t == 0)
-                ):
-                    # the are left in th list to be added to the next event
-                    t = 0
-                # if t==1, then that list is completed and the mean and length are recorded, so it is cleared for the next event
-                if t == 1:
-                    bin_current = []
-            w = w + 1
+    def append_bin_data(bin_current, bin_start):
+        """Append current bin data to result lists."""
+        if bin_current:
+            bin_length.append(len(bin_current))
+            bin_mean.append(np.mean(bin_current))
+            bin_index.append(bin_start)
+            level.append(
+                bin_indices[bin_start]
+                if bin_indices[bin_start] < len(bins_list)
+                else len(bins_list)
+            )
+            return []
 
-        # this section of code checks for when the same level is recorded a few times in a row, then adds them together
-        # it returns the mean, length, index, and levels lists as a list
+    for v, val in enumerate(filtered_sweepY):
+        if not bin_current or bin_indices[v] == bin_indices[bin_start]:
+            bin_current.append(val)
+        else:
+            # Handle completed bin
+            bin_current = append_bin_data(bin_current, bin_start)
+            bin_start = v
+            bin_current.append(val)
 
-        result = combine_repeates(bin_mean, bin_length, bin_index, level)
-        bin_mean = result[0]
-        bin_length = result[1]
-        bin_index = result[2]
-        level = result[3]
+        # Handle last value
+        if v == len(filtered_sweepY) - 1:
+            bin_current = append_bin_data(bin_current, bin_start)
 
-        # this finds the average length and weighted mean for each level
+    # Combine repeated levels
+    result = combine_repeates(bin_mean, bin_length, bin_index, level)
+    bin_mean, bin_length, bin_index, level = result
 
-        result = find_means(bins_list, level, bin_mean, bin_length)
-        global net_mean
-        net_mean = result[0]
+    # Calculate average length and weighted mean for each level
+    result = find_means(bins_list, level, bin_mean, bin_length)
+    net_mean = result[0]
 
     # Histogram code
 
